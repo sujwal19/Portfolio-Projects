b78a356b9ac0ca6be4e8114283b1e80dd9aac604
Sujwal <dsujwal24@gmail.com>
2026-02-26 19:59:25 +0545

    About, Experience, Serrvices Done

--- portfolio/src/components/resume/Resume.jsx
+++ portfolio/src/components/resume/Resume.jsx
@@ -1,7 +1,49 @@
 import "./resume.css";
+import Data from "./Data";
+import Card from "./Card";
 
 const Resume = () => {
-  return <div>Resume</div>;
+  return (
+    <section className="resume container section" id="resume">
+      <h2 className="section__title">Experience</h2>
+
+      <div className="resume__container grid">
+        <div className="timeline grid">
+          {Data.map((val, id) => {
+            if (val.category === "education") {
+              return (
+                <Card
+                  key={id}
+                  icon={val.icon}
+                  title={val.title}
+                  year={val.year}
+                  desc={val.desc}
+                  iconClass={val.iconClass}
+                />
+              );
+            }
+          })}
+        </div>
+
+        <div className="timeline grid">
+          {Data.map((val, index) => {
+            if (val.category === "experience") {
+              return (
+                <Card
+                  key={index}
+                  icon={val.icon}
+                  title={val.title}
+                  year={val.year}
+                  desc={val.desc}
+                  iconClass={val.iconClass}
+                />
+              );
+            }
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Resume;
